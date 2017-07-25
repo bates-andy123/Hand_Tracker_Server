d9e959cdca99efca4ba146decbe3f4351615d1b6
bates-andy123 <bates.andy123@gmail.com>
2017-07-25 01:40:24 +0000

    Added thread that resends serial data

--- src/FSM.py
+++ src/FSM.py
@@ -1,6 +1,6 @@
 def FSM(orientation, speed):
     # Change states
-    state_props = [(0,1,"None"), (1,-1,"None"), (2,1,"Left"), (3,1,"Right"), (4,-1,"Right"), (5,-1,"Left")]
+    state_props = [(0,1,"N"), (1,-1,"N"), (2,1,"L"), (3,1,"R"), (4,-1,"R"), (5,-1,"L")]
     if FSM.state == 0:
         FSM.state, FSM.throttle_dir, FSM.steer_dir = { \
             "Up":state_props[0], \
@@ -37,8 +37,8 @@ def FSM(orientation, speed):
             "Down":state_props[1], \
             "Left":state_props[4], \
             "Right":state_props[5]}[orientation]
-    return "%s,%s" % (FSM.steer_dir, FSM.throttle_dir * speed)
+    return "<%s,%d>" % (FSM.steer_dir, FSM.throttle_dir * speed)
 FSM.state = 0
 FSM.throttle_dir = 1
-FSM.steer_dir = "None"
+FSM.steer_dir = "N"
 FSM.speed = 0
--- src/server.py
+++ src/server.py
@@ -1,19 +1,32 @@
 from bottle import route, run, template
 from FSM import FSM
 import serial
+import threading
+import time
 
 arduino = serial.Serial("/dev/ttyS0", baudrate=9600)
+command = "<N,0>"
+
+def sending_thread():
+    print "Thread started"
+    while True: 
+        arduino.write(command)
+        arduino.flush()
+        time.sleep(0.05)
+
+thread = threading.Thread(target=sending_thread, args=())
+thread.start()
 
 @route('/<comm>')
 def index(comm):
+    global command
     items = comm.split(',')
     orientation = items[0]
     speed = int(items[1])
-    fsm = FSM(orientation, speed)
-    #arduino.write(fsm)
-    print fsm
+    command = FSM(orientation, speed)
+    print command
     return template('<b>command: {{comm}} length queue: {{length}}</b>', comm=comm, length = len(comm)) 
  
 #This must be it's own IP, not localhost
 #run(host='192.168.0.106', port=8080)
-run(host='192.168.1.85', port=8080)
+run(host='192.168.0.104', port=8080)
